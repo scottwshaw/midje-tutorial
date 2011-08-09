2b620a82200118058c7d98466d3ef1a131bd6689
Scott Shaw <scottwshaw@gmail.com>
2011-08-09 18:35:04 +1000

    Initial project setup for lein and midje

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject practice "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[lein-midje "1.0.3"]
+                     [midje "1.2.0"]
+                     [swank-clojure "1.3.2"]])
+
+
